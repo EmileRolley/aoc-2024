8358352729e5da42641b70af264ace10f756ae94
Emile Rolley <emile.rolley@tuta.io>
2024-12-03 20:13:40 +0100

    feat: day01 2/2

--- lib/day01.ml
+++ lib/day01.ml
@@ -1,28 +1,45 @@
 open Core
 open Day
 
-let get_input part =
-  match part with
-  | P1 -> "inputs/day01/part1.txt"
-  | P2 -> "inputs/day01/part2.txt"
+let get_input _ = "inputs/day01/inputs.txt"
+
+let parse_pairs lines =
+  List.map
+    ~f:
+      (Utils.try_parse
+         [ Utils.parse "%d   %d" (fun a b -> a, b); (* EOF *) Utils.parse "" (0, 0) ])
+    lines
 ;;
 
 let part_1 lines =
-  let l, r =
-    List.map
-      ~f:
-        (Utils.try_parse
-           [ Utils.parse "%d   %d" (fun a b -> a, b); Utils.parse "" (0, 0) ])
-      lines
-    |> List.unzip
-  in
+  let l, r = parse_pairs lines |> List.unzip in
   List.zip_exn (List.sort ~compare:Int.compare l) (List.sort ~compare:Int.compare r)
   |> List.fold_left ~init:0 ~f:(fun sum (l, r) -> sum + Int.abs (l - r))
   |> Int.to_string
 ;;
 
-let part part lines =
-  match part with
-  | P1 -> part_1 lines
-  | P2 -> "Not implemented"
+let part_2 lines =
+  let get_nb_occurences =
+    List.fold_left
+      ~init:(Map.empty (module Int))
+      ~f:(fun acc x ->
+        Map.change acc x ~f:(function
+          | None -> Some 1
+          | Some v -> Some (v + 1)))
+  in
+  let left, right = parse_pairs lines |> List.unzip in
+  let left_occurences = get_nb_occurences left in
+  let right_occurences = get_nb_occurences right in
+  left_occurences
+  |> Map.fold ~init:0 ~f:(fun ~key:num ~data:nb_occurences_in_left acc ->
+    match Map.find right_occurences num with
+    | None -> acc
+    | Some nb_occurences_in_right ->
+      (nb_occurences_in_left * (num * nb_occurences_in_right)) + acc)
+  |> Int.to_string
+;;
+
+let part = function
+  | P1 -> part_1
+  | P2 -> part_2
 ;;
