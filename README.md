0e2128ad2861253c211bb46e6047d50b4afb49e8
Emile Rolley <emile.rolley@tuta.io>
2024-12-07 17:46:56 +0100

    feat: day06 2/2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Advent of Code 2024 (11/48 ⭐)
+# Advent of Code 2024 (12/48 ⭐)
 
 My solutions to the [Advent of Code 2024](https://adventofcode.com/2024)
 puzzles in [OCaml](https://ocaml.org/).
--- bin/runnable.ml
+++ bin/runnable.ml
@@ -6,14 +6,12 @@ end
 
 module Make (D : Aoc.Day.DAY) : S = struct
   let run () =
-    let _ =
-      [ P1; P2 ]
-      |> List.map ~f:(fun p ->
-        D.get_input p
-        |> Aoc.Utils.read_input
-        |> D.part p
-        |> Printf.printf "Part %d: %d\n" (Aoc.Day.part_to_int p))
-    in
-    ()
+    [ P1; P2 ]
+    |> List.map ~f:(fun p ->
+      D.get_input p
+      |> Aoc.Utils.read_input
+      |> D.part p
+      |> Printf.printf "Part %d: %d\n" (Aoc.Day.part_to_int p))
+    |> ignore
   ;;
 end
